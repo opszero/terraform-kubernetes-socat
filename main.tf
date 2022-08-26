26cd8ae7888a4b81c7035d249193e6ed311c9b9d
Abhi Yerra <abhi@berkeley.edu>
2022-08-26 16:09:20 -0700

    Added metadata

--- main.tf
+++ main.tf
@@ -1,12 +1,28 @@
 resource "kubernetes_deployment_v1" "socat" {
   metadata {
     name = var.name
+
+    labels = {
+      name = var.name
+    }
   }
 
   spec {
     replicas = 1
 
+    selector {
+      match_labels = {
+        name = var.name
+      }
+    }
+
     template {
+      metadata {
+        labels = {
+          name = var.name
+        }
+      }
+
       spec {
         container {
           image = "alpine/socat"
